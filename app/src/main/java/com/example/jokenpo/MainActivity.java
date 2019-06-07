42af5edf3da23418c59d70bd5c6a475fc18b3bb8
Douglas Willamis <douglaswillamis@gmail.com>
2019-06-07 14:35:13 -0300

    melhorias no fluxo

--- app/src/main/java/com/example/jokenpo/MainActivity.java
+++ app/src/main/java/com/example/jokenpo/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.jokenpo;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.media.MediaPlayer;
@@ -18,17 +23,25 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView player;
     private ImageView adversario;
-
     private MediaPlayer musica;
-
+    private ImageButton pedra;
+    private ImageButton papel;
+    private ImageButton tesoura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
 
         this.player = findViewById(R.id.player);
         this.adversario = findViewById(R.id.advesario);
+        this.pedra = findViewById(R.id.pedra);
+        this.papel = findViewById(R.id.papel);
+        this.tesoura = findViewById(R.id.tesoura);
 
         this.musica = MediaPlayer.create(this, R.raw.alex_play);
         this.musica.setLooping(true);
@@ -50,23 +63,28 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()){
             case R.id.pedra :
                 this.player.setImageResource(R.drawable.pedra);
+                this.player.setScaleX(-1);
                 this.player.setTag(R.id.pedra);
                 animacao();
                 break;
             case R.id.papel :
                 this.player.setImageResource(R.drawable.papel);
+                this.player.setScaleX(-1);
                 this.player.setTag(R.id.papel);
                 animacao();
                 break;
             case R.id.tesoura :
                 this.player.setImageResource(R.drawable.tesoura);
+                this.player.setScaleX(-1);
                 this.player.setTag(R.id.tesoura);
                 animacao();
                 break;
         }
+        desativarBtn();
     }
 
     public void animacao(){
+
         final int TEMPO = 3950;
         this.musica.start();
         this.adversario.setImageResource(R.drawable.animacao);
@@ -86,25 +104,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sorteio() {
+
+        final int TEMPO = 1250;
         int indice = (new Random()).nextInt(3);
         switch (indice) {
             case 0 :
                 this.adversario.setImageResource(R.drawable.pedra);
                 this.adversario.setTag(R.id.pedra);
-                musica.pause();
-                musica.seekTo(0);
+                this.musica.pause();
+                this.musica.seekTo(0);
                 break;
             case 1 :
                 this.adversario.setImageResource(R.drawable.papel);
                 this.adversario.setTag(R.id.papel);
-                musica.pause();
-                musica.seekTo(0);
+                this.musica.pause();
+                this.musica.seekTo(0);
                 break;
             case 2 :
                 this.adversario.setImageResource(R.drawable.tesoura);
                 this.adversario.setTag(R.id.tesoura);
-                musica.pause();
-                musica.seekTo(0);
+                this.musica.pause();
+                this.musica.seekTo(0);
                 break;
         }
 
@@ -123,6 +143,32 @@ public class MainActivity extends AppCompatActivity {
         else {
             Toast.makeText(getApplicationContext(), "Você ganhou!", Toast.LENGTH_LONG).show();
         }
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                reiniciar();
+            }
+        }, TEMPO);
+
     }
 
+    public void reiniciar() {
+        this.player.setImageResource(R.drawable.interrogacao);
+        this.player.setScaleX(1);
+        this.adversario.setImageResource(R.drawable.interrogacao);
+        ativarBtn();
+    }
+
+    public void ativarBtn(){
+        this.pedra.setEnabled(true);
+        this.papel.setEnabled(true);
+        this.tesoura.setEnabled(true);
+    }
+
+    public void desativarBtn(){
+        this.pedra.setEnabled(false);
+        this.papel.setEnabled(false);
+        this.tesoura.setEnabled(false);
+    }
 }
